ad1571ac991c90267ec4710d0ab57f19987068c3
Erin Walker <erinb0317@hotmail.com>
2018-03-11 02:40:31 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,7 +12,7 @@ class Pokemon
   end
 
   def self.find(id, db)
-    db.execute("SELECT * FROM pokemon WHERE id = #{id.to_i}")
+    array = db.execute("SELECT * FROM pokemon WHERE id = #{id.to_i}")
     Pokemon.new(id:array[0][0], name:array[0][1], type:array[0][2], db:db)
   end
 end
