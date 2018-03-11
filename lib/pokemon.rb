35a29de6c75bad26b88c6061f5def016b0a8f540
Erin Walker <erinb0317@hotmail.com>
2018-03-11 02:53:44 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,10 +1,11 @@
 class Pokemon
-  attr_accessor :id, :name, :type, :db
+  attr_accessor :id, :name, :type, :db, :hp
 
-  def initialize(id:, name:, type:, db:)
+  def initialize(id:, name:, type:, db:, :hp)
     @id = id
     @name = name
     @type = type
+    @hp = hp
   end
 
   def self.save(name, type, db)
@@ -14,5 +15,9 @@ class Pokemon
   def self.find(id, db)
     array = db.execute("SELECT * FROM pokemon WHERE id = #{id.to_i}")
     Pokemon.new(id:array[0][0], name:array[0][1], type:array[0][2], db:db)
+  end
+
+  def alter_hp(hp, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", hp, self.id)
   end
 end
